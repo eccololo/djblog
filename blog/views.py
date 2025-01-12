fa94cbb6fbae54102e7e0c55dffd355228e58118
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-01-12 19:03:21 +0100

    Add starter code to djblog project.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def home(request):
+    """View for view all posts list page."""
+    context = {
+        
+    }
+    return render(request, "index.html", context)
+
+def posts(request):
+    """View for view all posts list page."""
+    return HttpResponse("ALl posts list.")
