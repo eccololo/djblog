dbbdb51b1fc466892aceca0b63601c5a71a606db
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-01-23 17:53:13 +0100

    Add String representation to models.

--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,22 @@ class Tag(models.Model):
 
     caption = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.caption
+
 
 class Author(models.Model):
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+
+    def get_full_name(self):
+        
+        return self.first_name + " " + self.last_name
+    
+    def __str__(self):
+        return self.get_full_name()
 
 
 class Post(models.Model):
